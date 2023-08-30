d64db94cd1bbda9936bff0aba5054349fbb5da35
Your Name <you@example.com>
2023-08-30 11:55:16 -0400

    django commit

---
+++ products/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('name',)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
+    
+
+class Products(models.Model):
+    name = models.TextField(blank=False)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    slug = models.SlugField()
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/{self.category.slug}/{self.slug}/'
+    
+    def get_image(self):
+        if self.image:
+            return 'http://127.0.0.1:8000' + self.image.url
+        return ''
